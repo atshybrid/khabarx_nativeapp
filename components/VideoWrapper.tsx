74defc74239312b96ff71bba665ac5753383d3ca
atshybrid <amoghnyatechsolutions@gmail.com>
2025-09-25 21:36:32 +0530

    Replace deprecated expo-av Video with expo-video in VideoWrapper.tsx

--- components/VideoWrapper.tsx
+++ components/VideoWrapper.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 // Temporary: still using expo-av until expo-video is installed.
 // After adding expo-video, switch import to: import { Video, ResizeMode, VideoProps } from 'expo-video';
-import { ResizeMode, Video, VideoProps } from 'expo-av';
+import { ResizeMode, Video, VideoProps } from 'expo-video';
 // Reference to keep linter from flagging unused re-export until migration
 void ResizeMode;
 
 export const VideoWrapper: React.FC<VideoProps> = (props) => <Video {...props} />;
 
-export { ResizeMode } from 'expo-av';
+export { ResizeMode } from 'expo-video';
